c4d7f4a97827b6237034449fbdaa2d0972c0903d
Gregor Gresak <design@gresak.net>
2017-09-22 10:47:29 +0200

    customization working again

--- custom-formats.php
+++ custom-formats.php
@@ -17,25 +17,37 @@ class GG_Custom_Formats {
 
 	protected $container_css_class = "formated-links";
 
-	protected $oembed;
-
 	protected $url;
 
 	protected $title;
 
 	public function __construct() {
-                //this functions remain in place for compatibility with formatted links plugin
+        //this functions remain in place for compatibility with formatted links plugin
 		add_shortcode( 'see', array($this,"recommend") );
 		add_shortcode( 'ggcte', array($this,"cte_button") );
+		
 		add_action( 'customize_register', array($this,'customizer') );
-		//add_action( 'wp_head', array($this,'set_css'));
-                add_filter('tiny_mce_before_init',array($this,'setup_styles'));
+		add_action( 'wp_head', array($this,'set_css'));
+		add_action( 'admin_head', array($this,'set_css'));
+        add_filter('tiny_mce_before_init',array($this,'setup_styles'));
 		add_filter( 'mce_buttons', array($this, 'register_tmce_buttons') );
-                add_filter('admin_init', array($this,'set_editor_styles'));
+        add_filter('admin_init', array($this,'set_editor_styles'));
+        //add_action( 'init',array($this,'register_string'));
 	}
-        
-        public function setup_styles($ed) {
-            $formats = array(  
+
+	public function register_string() {
+		if(function_exists('pll_register_string')) {
+        	pll_register_string('recommendation_string',"See also","custom-formats");
+        }
+	}
+    
+    /**
+     * Setup styles for tinymce
+     * @param  [type] $ed [description]
+     * @return [type]     [description]
+     */
+    public function setup_styles($ed) {
+        $formats = array(  
 		// Each array child is a format with it's own settings
 		array(  
 			'title' => 'CTA',  
@@ -63,18 +75,35 @@ class GG_Custom_Formats {
             $ed['style_formats'] = json_encode($formats);
             return $ed;
         }
-        
-        public function set_editor_styles() {
-            add_editor_style("css/editor-style.css");
-        }
+    
+    /**
+     * Deprecated
+     */
+    public function set_editor_styles() {
+        add_editor_style("css/editor-style.css");
+    }
 
-        public function cte_button($args,$content) {
+    /**
+     * Shortcode callback function for shortcode [ggcte]
+     * Deprecated, remains for compatibility
+     * @param  [type] $args    [description]
+     * @param  [type] $content [description]
+     * @return [type]          [description]
+     */
+    public function cte_button($args,$content) {
 		$data = $this->get_data($args, $content);
 		return '<div class="'.$this->container_css_class.'-button" style="text-align:center;">'
 		.'<a href="'.$data->url.'" class="turquoise-bg btn rounded  btn-lg" target="_blank"><b>'.$data->title.'</b></a>'
 				.'</div>';
 	}
 
+	/**
+	 * Shortcode callback for shortcode [see]
+	 * Deprecated, remains for compatibility
+	 * @param  array $args    shortcode args
+	 * @param  string $content shortcode content
+	 * @return string          html for shortcode
+	 */
 	public function recommend($args,$content="") {
 		$data = $this->get_data($args, $content);
 		return '<div class="'.$this->container_css_class.'"><b>'
@@ -89,6 +118,11 @@ class GG_Custom_Formats {
             return $buttons;
 	}
 
+	/**
+	 * Register customization for the recommendation "See also" string
+	 * @param  [type] $customize [description]
+	 * @return [type]            [description]
+	 */
 	public function customizer($customize) {
 		$customize->add_setting('recommendation_string',array("default"=>"See also"));
 		$customize->add_section('inline_recommendations', array(
@@ -108,20 +142,11 @@ class GG_Custom_Formats {
 			);
 	}
 
+	/**
+	 * Setup css for recommendation (See also)
+	 */
 	public function set_css() {
-		echo "<style>"
-				.".{$this->container_css_class}-button a {" 
-				."background-color: #1f8dd6;"
-				."border-radius: 0.5em;"
-			    ."display: inline-block;"
-			    ."line-height: 1em;"
-			    ."margin: 0.5em;"
-			    ."max-height: 4em;"
-			    ."padding: 16px 13px 17px;"
-			    ."text-align: center;"
-			    ."text-decoration: none;"
-			."} "
-			."</style>";
+		echo "\n".'<style type="text/css">.formatted-links.see::before { content: "'.get_theme_mod("recommendation_string",$this->recommendation_string).': " !important;font-weight: bold;color: black;}</style>'."\n";
 	}
 
 	protected function get_data($args,$content="") {
